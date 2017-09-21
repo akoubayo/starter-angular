c49155d8871d0d26160cb973bda2f3268391d0bf
Damien <damien@tilkee.com>
2017-09-21 14:49:14 +0200

    login component + router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule }                     from '@angular/platform-browser';
+import { NgModule }                          from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { AppRoutingModule }                  from './app-routing/app-routing.module';
+
+import { AppComponent }                      from './app.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
