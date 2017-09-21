babcd9bc8414010d637aef62df81513583c005e6
Damien <damien@tilkee.com>
2017-09-21 14:51:08 +0200

    login component + router2

---
+++ src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,36 @@
+import { NgModule }                                from '@angular/core';
+import { CommonModule }                            from '@angular/common';
+import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
+import { AppModule }                               from '../app.module';
+import { LoginComponent }                          from './../pages/login/login.component';
+import { FormsModule, ReactiveFormsModule }        from '@angular/forms';
+
+
+const appRoutes: Routes = [{
+    path: '',
+    component: LoginComponent
+}]
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+  ],
+  imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      {
+        useHash: true,
+        enableTracing: false, // <-- debugging purposes only
+        preloadingStrategy: PreloadAllModules, // To enable preloading of all lazy loaded modules
+      }
+    ),
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  exports: [
+    RouterModule,
+  ],
+  providers: []
+})
+
+export class AppRoutingModule { }
---
+++ src/app/pages/login/login.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+    public login = {
+        email:'',
+        passowrd:''
+    }
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+    public onSubmit = () => {
+        console.log(this.login)
+    }
+
+}
