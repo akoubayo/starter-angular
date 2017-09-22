8d9c9edc20b4d195af2223aa194aa3cae735e770
Damien <damien@tilkee.com>
2017-09-22 16:23:47 +0200

    Finish

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 
 import { User }              from './../../services/user/user'
 import { UserService }       from './../../services/user/user.service';
-import { Subject }           from 'rxjs/Subject';
 
 @Component({
   selector: 'app-dashboard',
--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,33 +1,30 @@
 import { Injectable } from '@angular/core';
 import { User }       from './user';
-import { Observable } from 'rxjs/Observable';
-import { Subject }             from 'rxjs/Subject';
 
 @Injectable()
 export class UserService {
 
-  private user:User; // Déclaration d'un user classique.
+    private user:User;
 
-  constructor() {
-        this.user = new User();
-  }
+    constructor() {
+        this.user = new User(); // Initialisation de notre User
+    }
 
-  // Renvoie l'adresse de l'objet User
-  public getUser = ():User => {
-      return this.user; // C'est la référence vers l'objet user qui est renvoyer
-  }
+    // Renvoie l'adresse de l'objet User
+    public getUser = ():User => {
+        return this.user; // C'est la référence vers l'objet user qui est renvoyer
+    }
 
   // Renvoie une copie de l'objet User
-  public getUserByCopy = ():User => {
-      let user_copy:User = new User();
-      for(let key in this.user) {
+    public getUserByCopy = ():User => {
+        let user_copy:User = new User();
+        for(let key in this.user) {
           user_copy[key] = this.user[key];
-      }
-      return user_copy;
-  }
+        }
+        return user_copy;
+    }
 
-  public setUser = (user) => {
-      this.user = user;
-  }
-
+    public setUser = (user) => {
+        this.user = user;
+    }
 }
